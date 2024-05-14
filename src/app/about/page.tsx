fb36b347aa3db98d815c6fdc2fe09b5f0df64d38
imdamiandev <imdamian.dev@gmail.com>
2024-05-14 16:13:35 -0400

    config font

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+import { titleFont } from "@/config/fonts";
+
+export default function About() {
+  return (
+    <main>
+      <h1 className={titleFont.className}>About page</h1>
+    </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import {Button, ButtonGroup} from "@nextui-org/button";
+import { titleFont } from "@/config/fonts";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>hola mundo</h1>
+    <main>
+      <h1 className={titleFont.className}>Home</h1>
     </main>
   );
 }
