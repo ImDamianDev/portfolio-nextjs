e469358f45bd03b5486ddc38759365cf54fbd188
imdamiandev <imdamian.dev@gmail.com>
2024-05-14 14:25:48 -0400

    install and config nextui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+// 1. import `NextUIProvider` component
+import { NextUIProvider } from "@nextui-org/react";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <NextUIProvider>
+        <body className={inter.className}>{children}</body>
+      </NextUIProvider>
     </html>
   );
 }
