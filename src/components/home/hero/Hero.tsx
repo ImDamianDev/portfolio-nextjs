ba6210453cd6863c54fd0d141c50cb239869b00e
imdamiandev <imdamian.dev@gmail.com>
2024-05-17 19:29:59 -0400

    add animations hero

--- src/components/home/hero/Hero.tsx
+++ src/components/home/hero/Hero.tsx
@@ -24,7 +24,11 @@ export const Hero = () => {
 
 
     return (
-        <div id="hero" className='min-h-screen flex flex-col-reverse md:flex-row justify-center'>
+        <div id="hero" className='relative min-h-screen flex flex-col-reverse md:flex-row justify-center'>
+
+            <div className='absolute w-20 h-20 rounded-full bg-primary-200 top-20 left-10 md:w-28 md:h-28 md:top-32 lg:left-20 lg:top-32 xl:left-40 xl:top-40 2xl:w-48 2xl:h-48 transition-all opacity-60 -z-10'>
+
+            </div>
 
             <div className="grid content-center p-5">
 
@@ -32,8 +36,8 @@ export const Hero = () => {
                     <p className='text-2xl mb-1 pl-1 font-semibold text-primary-700'>
                         Hola, mi nombre es
                     </p>
-                    <h1 className="text-5xl lg:text-7xl font-bold antialiased">
-                        Damián Cortés
+                    <h1 className="text-5xl lg:text-7xl font-bold antialiased pulse">
+                        Damian Cortés
                     </h1>
                 </div>
 
