78e99f9df251568963f310612bebe52d96bd80f3
imdamiandev <imdamian.dev@gmail.com>
2024-05-17 21:06:05 -0400

    modify animation hero

--- src/components/home/hero/Hero.tsx
+++ src/components/home/hero/Hero.tsx
@@ -24,19 +24,18 @@ export const Hero = () => {
 
 
     return (
-        <div id="hero" className='relative min-h-screen flex flex-col-reverse md:flex-row justify-center'>
+        <div id="hero" className='relative min-h-screen flex flex-col-reverse md:flex-row justify-center items-center'>
 
-            <div className='absolute w-20 h-20 rounded-full bg-primary-200 top-20 left-10 md:w-28 md:h-28 md:top-32 lg:left-20 lg:top-32 xl:left-40 xl:top-40 2xl:w-48 2xl:h-48 transition-all opacity-60 -z-10'>
-
+            <div className='absolute w-20 h-20 rounded-full bg-primary-300 top-20 left-10 md:w-28 md:h-28 md:top-32 lg:left-20 lg:top-32 xl:left-40 xl:top-40 2xl:w-48 2xl:h-48 transition-all opacity-60 -z-10 pulse'>
             </div>
 
-            <div className="grid content-center p-5">
+            <div className="grid content-center p-5 h-fit">
 
                 <div>
                     <p className='text-2xl mb-1 pl-1 font-semibold text-primary-700'>
                         Hola, mi nombre es
                     </p>
-                    <h1 className="text-5xl lg:text-7xl font-bold antialiased pulse">
+                    <h1 className="text-5xl lg:text-7xl font-bold antialiased">
                         Damian Cortés
                     </h1>
                 </div>
@@ -52,7 +51,7 @@ export const Hero = () => {
 
             </div>
 
-            <div className='flex items-center p-7'>
+            <div className='relative flex items-center p-7'>
                 <Image
                     src='/profile_picture/profile_picture_imdamiandev.png'
                     alt="hero-image-imdamiandev"
@@ -60,6 +59,8 @@ export const Hero = () => {
                     height={500}
                     className='rounded-full'
                 />
+                <div className='absolute w-28 h-20 rounded-full bg-primary-400 bottom-10 right-10 drop-shadow-md bounce'>
+                </div>
             </div>
         </div>
     )
