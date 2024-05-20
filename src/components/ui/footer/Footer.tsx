620e808a1a765bca39656a9dec2b6b60aef20cc1
imdamiandev <imdamian.dev@gmail.com>
2024-05-20 12:11:27 -0400

    modify style footer component

--- src/components/ui/footer/Footer.tsx
+++ src/components/ui/footer/Footer.tsx
@@ -1,16 +1,20 @@
-import { FaGithub, FaLinkedin, FaMailchimp } from "react-icons/fa";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { IoMdMail } from "react-icons/io";
 
 export const Footer = () => {
     return (
-        <footer className="py-7">
+        <footer className="py-12 text-center text-sm font-light">
             <div className="flex flex-row justify-center mb-3 gap-3">
-                <a href="https://github.com/ImDamianDev/" className="text-primary-500 hover:"><FaGithub size="2.5em" /></a>
-                <a href="https://www.linkedin.com/in/imdamian-dev/" className="text-primary-500"><FaLinkedin size="2.5em" /></a>
-                <a href="mailto:imdamian.dev@gmail.com" className="text-primary-500"><IoMdMail size="2.5em" /></a>
+                <a href="https://github.com/ImDamianDev/" className="hover:scale-105"><FaGithub size="2em" /></a>
+                <a href="https://www.linkedin.com/in/imdamian-dev/" className="hover:scale-105"><FaLinkedin size="2em" /></a>
+                <a href="mailto:imdamian.dev@gmail.com" className="hover:scale-105"><IoMdMail size="2em" /></a>
             </div>
-            <p className="text-center text-xl font-semibold">Con <span className="text-red-500">&#x2665;</span> desde Chile.</p>
-            <span className="flex justify-center text-lg">{new Date().getFullYear()}</span>
+
+            <span className="h-1 rounded w-[200px] my-5 mx-auto bg-secondary flex" />
+
+            <p>Con <span className="text-red-400">&#x2665;</span> desde Chile.</p>
+
+            <span className="flex justify-center">{new Date().getFullYear()}</span>
         </footer>
     );
 };
