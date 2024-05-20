b1f68305e4fcb1fcc1063d5bd8f4b3e9a90a3cf6
imdamiandev <imdamian.dev@gmail.com>
2024-05-19 22:07:26 -0400

    test new mt config on hero components

--- src/components/home/hero/Hero.tsx
+++ src/components/home/hero/Hero.tsx
@@ -28,7 +28,7 @@ export const Hero = () => {
 
             <div className='absolute w-20 h-20 rounded-full bg-secondary top-20 left-10 md:w-28 md:h-28 md:top-32 lg:left-20 lg:top-32 xl:left-40 xl:top-40 2xl:w-48 2xl:h-48 transition-all opacity-60 -z-10 pulse' />
 
-            <div className="grid content-center text-center md:text-left p-5 h-fit w-full md:w-auto lg:mr-5">
+            <div className="grid content-center text-center md:text-left p-5 h-fit w-full md:w-auto lg:mr-5 md:-mt-24">
 
                 <div>
                     <p className={`${titleFont.className} text-xl lg:text-3xl mb-1 pl-0.5`}>
@@ -46,7 +46,7 @@ export const Hero = () => {
 
             </div>
 
-            <div className='basis-1/4 md:basis-2/4 lg:basis-auto relative flex items-center px-14 md:px-0 -mt-36 md:mt-0'>
+            <div className='basis-1/4 md:basis-2/4 lg:basis-auto relative flex items-center px-14 md:px-0 -mt-40 md:-mt-24'>
                 <Image
                     src='/profile-picture/profile-picture.png'
                     alt="hero-image-imdamiandev"
@@ -56,7 +56,7 @@ export const Hero = () => {
                 />
             </div>
 
-            <div className='absolute w-12 h-20 rounded-full border-3 border-secondary bottom-20'>
+            <div className='absolute w-12 h-20 rounded-full border-3 border-secondary bottom-28'>
                 <FaArrowDown size={25} className='text-secondary absolute bottom-3 left-[9px]'/>
             </div>
         </div>
