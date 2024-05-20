845304d910ff3f26f079d19d3305a09684d3ea4a
imdamiandev <imdamian.dev@gmail.com>
2024-05-20 12:19:53 -0400

    change from "defaut theme" to dark

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -7,7 +7,7 @@ import {ThemeProvider as NextThemesProvider} from "next-themes";
 export function Providers({children}: { children: React.ReactNode }) {
   return (
     <NextUIProvider>
-      <NextThemesProvider attribute="class" defaultTheme="light">
+      <NextThemesProvider attribute="class" defaultTheme="dark">
         {children}
       </NextThemesProvider>
     </NextUIProvider>
